159e748eaeed18958785b849bfd4dd30e24b5619
JaysonM01 <jayson_malicse55555@yahoo.com>
2025-04-25 12:57:20 +0800

    Activity: Debugging Code [Start]
     - copy the code provided
     - code divides a number by 0
     - expected error prompt

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Method to divide two numbers
+using System;
+public class Program
+{
+public static int DivideNumbers(int numerator, int denominator)
+{
+    int result = numerator / denominator;
+    return result;
+}
+
+public static void Main()
+{
+    // Attempt to divide 10 by 0
+    int result = DivideNumbers(10, 0);
+    Console.WriteLine("The result is: " + result);
+}
+}

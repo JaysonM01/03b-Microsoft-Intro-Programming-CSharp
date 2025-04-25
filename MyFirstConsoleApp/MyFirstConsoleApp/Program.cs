6ee5fd6bff7c2e0ffce6b6bbaa9db55306051555
JaysonM01 <jayson_malicse55555@yahoo.com>
2025-04-25 14:51:07 +0800

    Create ToDoList Program [Start-End]
     - Created a ToDoList Program that adds task, view tasks, and edit task and mark as completed
     - When selecting task that is not available, will output an error message
     - Program is already done in the past

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -1,22 +1,41 @@
-﻿public class Program
+﻿public class ToDoList
 {
-    public static int FindMax(int[] numbers)
+    public static string[] tasks = new string[10];
+    public static int taskCount = 0;
+    public static void AddTask()
     {
-        int max = int.MinValue;
-        for (int i = 0; i < numbers.Length; i++)
+        Console.WriteLine("What is your task?");
+        for (int i = 0; i == taskCount; taskCount++)
         {
-            if (numbers[i] > max)
-            {
-                max = numbers[i];
-            }
+            tasks[i] = Console.ReadLine();
         }
-        return max;
     }
-
-    public static void Main()
+    public static void ViewTasks()
     {
-        int[] myNumbers = { -5, -10, -3, -8, -2 };
-        int maxNumber = FindMax(myNumbers);
-        Console.WriteLine("The maximum number is: " + maxNumber);
+        for (int i = 0; i + 1 <= taskCount; i++)
+        {
+            Console.WriteLine(i+1 + ". " + tasks[i]);
+        }
+    }
+    public static void CompleteTask()
+    {
+        Console.WriteLine("Select a task you completed.");
+        int taskSelect = (int.Parse(Console.ReadLine())) - 1;
+        if (taskSelect >= taskCount)
+        {
+            Console.WriteLine("Selected task does not exist. Please try again.");
+        }
+        else
+        {
+            string updateTask = tasks[taskSelect] + " [Completed]";
+            tasks[taskSelect] = updateTask;
+            Console.WriteLine(taskSelect+1 + ". " + tasks[taskSelect]);
+        }
+    }
+    public static void Main(string[] args)
+    {
+        AddTask();
+        ViewTasks();
+        CompleteTask();
     }
 }

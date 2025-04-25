65891ff30f3c2695d084de63f7139600fc43f5e0
JaysonM01 <jayson_malicse55555@yahoo.com>
2025-04-25 13:38:12 +0800

    Problem to Complete 2
     - copied the provided code
     - finds the highest value in array but output is wrong

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -1,14 +1,22 @@
 ﻿public class Program
 {
-// Method to calculate the final price after a discount
-public static double ApplyDiscount(double price, double discountPercentage)
-{
-    return price - (price * discountPercentage * 0.01);
-}
+    public static int FindMax(int[] numbers)
+    {
+        int max = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+        }
+        return max;
+    }
 
-public static void Main()
-{
-    double finalPrice = ApplyDiscount(1000, 15);
-    Console.WriteLine("The final price is: " + finalPrice);
-}
+    public static void Main()
+    {
+        int[] myNumbers = { -5, -10, -3, -8, -2 };
+        int maxNumber = FindMax(myNumbers);
+        Console.WriteLine("The maximum number is: " + maxNumber);
+    }
 }

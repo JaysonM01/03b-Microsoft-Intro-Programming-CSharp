f9b187744be779a01cdeb0322052cf9d895397f3
JaysonM01 <jayson_malicse55555@yahoo.com>
2025-04-25 16:14:41 +0800

    Polymorphism with Lists of Base Types and Interfaces [End]
     - create a list of Animal object including instances of Dog and Cat
     - use a loop to call MakeSound method on each object

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -44,7 +44,14 @@ public class Program
         Persian.MakeSound();
         Pitbull.Eat();
         Persian.Eat();
+        List<Animal> animals = new List<Animal>();
+        animals.Add(new Dog());
+        animals.Add(new Cat());
         
+        foreach (Animal animal in animals)
+        {
+            animal.MakeSound();
+        }
     }
 }
 

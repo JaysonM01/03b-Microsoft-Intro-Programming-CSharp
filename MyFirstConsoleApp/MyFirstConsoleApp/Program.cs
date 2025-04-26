90127b6a46dfff04960db6983143658ffb2d7f3e
JaysonM01 <jayson_malicse55555@yahoo.com>
2025-04-26 17:05:31 +0800

    Run Multiple Methods
     - create a second Async method
     - updated the Main method to run in async both tasks

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -6,14 +6,23 @@ public class Program
     {
         Console.WriteLine("Downloading Data...");
         await Task.Delay(3000);
-        Console.WriteLine("Downloading Finished.");
+        Console.WriteLine("Downloading Data Finished.");
     }
+
+    public async Task DownloadDataAsync2()
+    {
+        Console.WriteLine("Downloading Data2...");
+        await Task.Delay(1000);
+        Console.WriteLine("Downloading Data2 Finished.");
+    }
+
 
     public static async Task Main()
     {
         Program program = new Program();
-        await program.DownloadDataAsync();
+        Task task1 = program.DownloadDataAsync();
+        Task task2 = program.DownloadDataAsync2();
+        await Task.WhenAll(task1, task2);
         Console.WriteLine("Program Done.");
-    
     }
 }

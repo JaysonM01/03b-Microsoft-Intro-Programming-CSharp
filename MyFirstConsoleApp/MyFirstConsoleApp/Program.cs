682be4fce342b5879fa9de3a918fcaa03f8351c8
JaysonM01 <jayson_malicse55555@yahoo.com>
2025-04-26 16:55:12 +0800

    Run Async Method
     - changed method to Task
     - call DownloadDataAsync method with await

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -1,10 +1,19 @@
-﻿public class Program
+﻿using System.Threading.Tasks;
+
+public class Program
 {
-    public async void DownloadDataAsync()
+    public async Task DownloadDataAsync()
     {
         Console.WriteLine("Downloading Data...");
-        await Task.Delay(2000);
+        await Task.Delay(3000);
         Console.WriteLine("Downloading Finished.");
     }
+
+    public static async Task Main()
+    {
+        Program program = new Program();
+        await program.DownloadDataAsync();
+        Console.WriteLine("Program Done.");
     
+    }
 }

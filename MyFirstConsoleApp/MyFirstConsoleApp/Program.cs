753a848fbd7c59d3e76384ae85d25046dec2039d
JaysonM01 <jayson_malicse55555@yahoo.com>
2025-04-28 09:46:55 +0800

    Problem #1 Answers

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -1,14 +1,14 @@
 ﻿public class Program
 {
-public _____ Task<string> DownloadFileAsync(string fileName)
+public async Task<string> DownloadFileAsync(string fileName)
 {
     Console.WriteLine($"Starting download of {fileName}...");
-    _____ Task.Delay(3000); // Simulate a 3-second download time
+    await Task.Delay(3000); // Simulate a 3-second download time
     Console.WriteLine($"Completed download of {fileName}.");
     return $"{fileName} content";
 }
 
-public _____ Task DownloadFilesAsync()
+public async Task DownloadFilesAsync()
 {
     // Start downloading "File1.txt" and "File2.txt" concurrently
     var downloadTask1 = DownloadFileAsync("File1.txt");
@@ -16,13 +16,13 @@ public _____ Task DownloadFilesAsync()
 
 
     // Wait for both downloads to complete
-    _____ Task.WhenAll(downloadTask1, downloadTask2);
+    await Task.WhenAll(downloadTask1, downloadTask2);
     Console.WriteLine("All downloads completed.");
 }
 
-public static _____ Task Main(string[] args)
+public static async Task Main(string[] args)
 {
     Program program = new Program();
-    _____ program.DownloadFilesAsync();
+    await program.DownloadFilesAsync();
 }
 }

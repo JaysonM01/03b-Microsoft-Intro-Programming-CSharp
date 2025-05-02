f21df85b7988696f081746bfd6a4e30e844552e6
JaysonM01 <jayson_malicse55555@yahoo.com>
2025-05-02 11:00:42 +0800

    Simulate with Console Statement
     - print message after the delay in PerformLongOperationAsync method

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -5,6 +5,7 @@ public class Program
     public static async Task PerformLongOperationAsync()
     {
         await Task.Delay(1000);
+        Console.WriteLine("Operation Finished");
     }
     public static async Task Main()
     {
